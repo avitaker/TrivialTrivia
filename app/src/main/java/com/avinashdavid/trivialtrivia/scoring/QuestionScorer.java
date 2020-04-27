4581ca4ceb2a70e339a3b9d6a81868ec6a611690
Carrie <clipsong@gmail.com>
2020-04-26 21:59:50 -0700

    Added testing for the parcelable QuestionScorerTest thats used to convert the state of the object into a byte stream

--- app/src/main/java/com/avinashdavid/trivialtrivia/scoring/QuestionScorer.java
+++ app/src/main/java/com/avinashdavid/trivialtrivia/scoring/QuestionScorer.java
@@ -41,22 +41,19 @@ public class QuestionScorer implements Parcelable {
         this.timeTaken = in.readInt();
         this.mCorrectAnswer = in.readInt();
         this.mChosenAnswer = in.readInt();
-        boolean[] correct = new boolean[1];
-        in.readBooleanArray(correct);
-        this.mCorrect = correct[0];
+        byte trueOrNot = in.readByte();
+        this.mCorrect = (trueOrNot == 0);
     }
 
     @Override
     public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeString("Hello");
-//       parcel.writeInt(mQuestionNumber);
-//        parcel.writeInt(mCategory);
-//        parcel.writeInt(timeTaken);
-//        parcel.writeInt(mCorrectAnswer);
-//        parcel.writeInt(mChosenAnswer);
-//        boolean[] correct = new boolean[1];
-//        correct[0] = mCorrect;
-//        parcel.writeBooleanArray(correct);
+        parcel.writeInt(mQuestionNumber);
+        parcel.writeInt(mCategory);
+        parcel.writeInt(timeTaken);
+        parcel.writeInt(mCorrectAnswer);
+        parcel.writeInt(mChosenAnswer);
+        byte correct = (byte) (( mCorrect ) ? 0 : 1);
+        parcel.writeByte( correct );
     }
 
     @Override
@@ -70,6 +67,7 @@ public class QuestionScorer implements Parcelable {
             return new QuestionScorer(parcel);
         }
 
+        // There to deserialize an array of Parcelable QuestionScorers
         @Override
         public Object[] newArray(int i) {
             return new QuestionScorer[i];
@@ -97,6 +95,10 @@ public class QuestionScorer implements Parcelable {
     }
 
     public void setTimeTaken(int timeTaken) {
-        this.timeTaken = timeTaken;
+        if( timeTaken >= 0 ){
+            this.timeTaken = timeTaken;
+        }
+        else
+            throw new IllegalStateException("ERROR: Time must be a positive integer.");
     }
 }
--- app/src/test/java/com/avinashdavid/trivialtrivia/scoring/QuestionScorerTest.java
+++ app/src/test/java/com/avinashdavid/trivialtrivia/scoring/QuestionScorerTest.java
@@ -1,9 +1,13 @@
 package com.avinashdavid.trivialtrivia.scoring;
 
 import android.os.Parcel;
+import com.avinashdavid.trivialtrivia.MockParcel;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class QuestionScorerTest {
 
@@ -19,8 +23,33 @@ public class QuestionScorerTest {
     }
 
     @Test
-    public void getCategory() {
-        assertEquals(3, qsNoTimeCorrect.getCategory());
+    public void writeToParcelThenCreateFromParcel() {
+        //Create a mock parcel and write the contents of qsWithTimeCorrect to Parcel
+        Parcel parcel = MockParcel.obtain();
+        qsWithTimeCorrect.writeToParcel(parcel, qsWithTimeCorrect.describeContents());
+        parcel.setDataPosition(0);
+
+        //Create a new QuestionScorer from the parcel contents
+        QuestionScorer createdFromParcel = (QuestionScorer)  QuestionScorer.CREATOR.createFromParcel(parcel);
+
+        //Compare what wat created from the Parcel
+        assertEquals(2, createdFromParcel.getQuestionNumber());
+        assertEquals(4, createdFromParcel.getCategory());
+        assertEquals( 100, createdFromParcel.getTimeTaken());
+        assertEquals(3, createdFromParcel.getChosenAnswer());
+        assertEquals( true, createdFromParcel.getQuestionEvaluation() );
+    }
+
+    @Test
+    public void newArrayOfTheParcelableClass() {
+        QuestionScorer[] origQS = { qsWithTimeCorrect, qsNoTimeIncorrect };
+        QuestionScorer[] newArrayFromQS = (QuestionScorer[]) QuestionScorer.CREATOR.newArray(2);
+        System.arraycopy(origQS, 0, newArrayFromQS,0,2);
+
+        for ( int i = 0; i < 2; i++ ) {
+            assertEquals(origQS[i].getCategory(), newArrayFromQS[i].getCategory());
+            assertEquals(origQS[i].getQuestionNumber(), newArrayFromQS[i].getQuestionNumber());
+        }
     }
 
     @Test
@@ -33,6 +62,24 @@ public class QuestionScorerTest {
     public void setTimeTaken() {
         qsNoTimeCorrect.setTimeTaken(88);
         assertEquals(88, qsNoTimeCorrect.getTimeTaken());
+    }
+
+    @Test
+    public void setTimeTakenIllegalStateException() {
+        try {
+            //Should throw exception from negative number
+            qsNoTimeCorrect.setTimeTaken(-88);
+            fail("Expected exception has not been thrown");
+
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(), is("ERROR: Time must be a positive integer."));
+        }
+    }
+
+    @Test
+    public void getCategory() {
+        assertEquals(3, qsNoTimeCorrect.getCategory());
+        assertEquals(4, qsWithTimeCorrect.getCategory());
     }
 
     @Test
@@ -50,25 +97,4 @@ public class QuestionScorerTest {
         assertTrue( qsNoTimeCorrect.getQuestionEvaluation()  );
         assertFalse( qsNoTimeIncorrect.getQuestionEvaluation() );
     }
-
-
-    @Test
-    public void writeToParcel() {
-        QuestionScorer qs = new QuestionScorer(7, 3, 1, 1);
-        Parcel parcel = Parcel.obtain();
-        qs.writeToParcel(parcel, 0);
-        parcel.setDataPosition(0);
-        QuestionScorer createdFromParcel = (QuestionScorer)  QuestionScorer.CREATOR.createFromParcel(parcel);
-
-        assertEquals(3, createdFromParcel.getCategory());
-        assertEquals(7, createdFromParcel.getQuestionNumber());
-    }
-
-
-//    @Test
-//    public void describeContents() {
-//
-//    }
-
-
 }
