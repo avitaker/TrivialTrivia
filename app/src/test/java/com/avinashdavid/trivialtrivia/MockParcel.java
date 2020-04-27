0801408bd1957ff212d14334b312d3786efca83e
Carrie <clipsong@gmail.com>
2020-04-26 21:57:37 -0700

    Added MockParcel inorder to test Parcelable classes that are used to pass information between activities

---
+++ app/src/test/java/com/avinashdavid/trivialtrivia/MockParcel.java
@@ -0,0 +1,77 @@
+package com.avinashdavid.trivialtrivia;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import java.util.LinkedList;
+import java.util.List;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyByte;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyFloat;
+import static org.mockito.ArgumentMatchers.booleanThat;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MockParcel {
+
+    @NonNull
+    public static Parcel obtain() {
+        return new MockParcel().mParcel;
+    }
+
+    private int mPosition = 0;
+    private List<Object> mStore = new LinkedList<>();
+    private Parcel mParcel = mock(Parcel.class);
+
+    private MockParcel() {
+        setupWrites();
+        setupReads();
+        setupOthers();
+    }
+
+    private void setupWrites() {
+        final Answer<Object> answer = new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock i) throws Throwable {
+                final Object arg = i.getArgument(0);
+                mStore.add(arg);
+                return arg;
+            }
+        };
+        doAnswer(answer).when(mParcel).writeByte(anyByte());
+        doAnswer(answer).when(mParcel).writeInt(anyInt());
+        doAnswer(answer).when(mParcel).writeString(anyString());
+        doAnswer(answer).when(mParcel).writeParcelable( any(Parcelable.class), anyInt() );
+        doAnswer(answer).when(mParcel).writeFloat(anyFloat());
+    }
+
+    private void setupReads() {
+        final Answer<Object> answer = new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock i) throws Throwable {
+                return mStore.get(mPosition++);
+            }
+        };
+        when(mParcel.readByte()).thenAnswer(answer);
+        when(mParcel.readInt()).thenAnswer(answer);
+        when(mParcel.readString()).thenAnswer(answer);
+        when(mParcel.readParcelable(any(ClassLoader.class))).then(answer);
+        when(mParcel.readFloat()).thenAnswer(answer);
+    }
+
+    private void setupOthers() {
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock i) throws Throwable {
+                mPosition = i.getArgument(0);
+                return null;
+            }
+        }).when(mParcel).setDataPosition(anyInt());
+    }
+
+}
