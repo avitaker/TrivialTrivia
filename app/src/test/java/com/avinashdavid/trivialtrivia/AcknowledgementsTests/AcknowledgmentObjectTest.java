b0341b8a0e5d7a2f5e5153c60918a27eb35f40bd
Laura Larson <larsonlaura@seattleu.edu>
2020-04-27 14:19:42 -0700

    Added code to test method AcknowledgementObject_GetNameTest()

--- app/src/test/java/com/avinashdavid/trivialtrivia/AcknowledgementsTests/AcknowledgmentObjectTest.java
+++ app/src/test/java/com/avinashdavid/trivialtrivia/AcknowledgementsTests/AcknowledgmentObjectTest.java
@@ -3,21 +3,18 @@ package com.avinashdavid.trivialtrivia.AcknowledgementsTests;
 import android.net.Uri;
 
 import com.avinashdavid.trivialtrivia.acknowledgments.AcknowledgmentObject;
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+
+
 
 public class AcknowledgmentObjectTest {
 
     @Before
     public void setUp() throws Exception {
-        String name = "Bogus";
-        String link = "http://www.bogus.com/";
-        Uri uri = Uri.parse(link);
-        AcknowledgmentObject obj = new AcknowledgmentObject(name, uri);
     }
 
     @After
@@ -26,7 +23,13 @@ public class AcknowledgmentObjectTest {
 
     @Test
     public void AcknowledgementObject_GetNameTest () {
-
+        String name = "Bogus";
+        String link = "http://www.bogus.com/";
+        Uri uri = Uri.parse(link);
+        AcknowledgmentObject obj = new AcknowledgmentObject(name, uri);
+        String actual = obj.getName();
+        assertNotNull(obj);
+        assertEquals("Bogus", actual);
     }
 
     @Test
