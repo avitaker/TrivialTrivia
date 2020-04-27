151eeaeea3a4ec201d05c75ed1a952160a56d0dd
Carrie <clipsong@gmail.com>
2020-04-27 10:22:06 -0700

    Add new test class for IndividualQuestion

---
+++ app/src/test/java/com/avinashdavid/trivialtrivia/questions/IndividualQuestionTest.java
@@ -0,0 +1,23 @@
+package com.avinashdavid.trivialtrivia.questions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IndividualQuestionTest {
+
+    @Test
+    public void getCategoryList() {
+        fail("Not implemented");
+    }
+
+    @Test
+    public void writeToParcel() {
+        fail("Not implemented");
+    }
+
+    @Test
+    public void describeContents() {
+        fail("Not implemented");
+    }
+}
