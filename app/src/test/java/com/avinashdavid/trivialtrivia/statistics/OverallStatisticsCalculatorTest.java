c4d48111d5c20a3ad2f94576d33edd29f0c93d1a
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2020-04-27 11:52:01 -0700

    Add a unit test for getNumberOfPerfectQuizzes to check for scorelist and quizsizelist don't have the same length.

--- app/src/test/java/com/avinashdavid/trivialtrivia/statistics/OverallStatisticsCalculatorTest.java
+++ app/src/test/java/com/avinashdavid/trivialtrivia/statistics/OverallStatisticsCalculatorTest.java
@@ -2,6 +2,7 @@ package com.avinashdavid.trivialtrivia.statistics;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -23,7 +24,7 @@ public class OverallStatisticsCalculatorTest
     }
 
     @Test
-    public void GetNumberOfPerfectQuizzesWithNullQuizSizeListThrows()
+    public void getNumberOfPerfectQuizzesWithNullQuizSizeListThrows()
     {
         int[] scorelist = new int[]{7, 3};
         try
@@ -35,5 +36,15 @@ public class OverallStatisticsCalculatorTest
             assertTrue(true);
         }
         fail("Expected IllegalArgumentException");
+    }
+
+    @Test
+    public void getNumberOfPerfectQuizzesWithNotEqualInputLists()
+    {
+        int[] scorelist = new int[]{3, 7};
+        int[] quizsizelist = new int[]{2};
+        double actual = OverallStatisticsCalculator.getNumberOfPerfectQuizzes(scorelist, quizsizelist);
+        double expected = -1;
+        assertEquals(expected, actual, 0.0001);
     }
 }
