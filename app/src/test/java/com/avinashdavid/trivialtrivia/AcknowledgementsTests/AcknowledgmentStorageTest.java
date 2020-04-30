afe3e2ddd801cbd2347f3fefd210a2b75764751a
Laura Larson <larsonlaura@seattleu.edu>
2020-04-30 13:19:30 -0700

    Added UT AcknowledgementStorage_getQuestionsAcknowledgmentObjectsTest()

--- app/src/test/java/com/avinashdavid/trivialtrivia/AcknowledgementsTests/AcknowledgmentStorageTest.java
+++ app/src/test/java/com/avinashdavid/trivialtrivia/AcknowledgementsTests/AcknowledgmentStorageTest.java
@@ -1,15 +1,25 @@
 package com.avinashdavid.trivialtrivia.AcknowledgementsTests;
 
+import com.avinashdavid.trivialtrivia.acknowledgments.AcknowledgmentObject;
+import com.avinashdavid.trivialtrivia.acknowledgments.AcknowledgmentStorage;
+import android.net.Uri;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
+@RunWith(RobolectricTestRunner.class)
 public class AcknowledgmentStorageTest {
 
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
     }
 
     @After
@@ -18,11 +28,16 @@ public class AcknowledgmentStorageTest {
 
     @Test
     public void AcknowledgementStorage_getQuestionsAcknowledgmentObjectsTest(){
+        AcknowledgmentStorage storage = new AcknowledgmentStorage();
+        ArrayList<AcknowledgmentObject> list = storage.getQuestionsAcknowledgmentObjects();
+        AcknowledgmentObject item = new AcknowledgmentObject("Mental Floss", "http://mentalfloss.com/");
 
+        assertNotNull(list);
+        assertEquals(item.getName(), list.get(1).getName());
     }
 
     @Test
     public void AcknowledgementStorage_getImagesAcknowledgmentObjectsTest(){
-
+        
     }
 }
