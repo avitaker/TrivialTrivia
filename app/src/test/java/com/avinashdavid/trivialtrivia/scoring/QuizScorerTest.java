906e0925ace948c6bf355b13ef96feacd8acf9fa
Carrie <clipsong@gmail.com>
2020-05-04 13:01:28 -0700

    Fix for testing static factory in quizscorer

--- app/src/test/java/com/avinashdavid/trivialtrivia/scoring/QuizScorerTest.java
+++ app/src/test/java/com/avinashdavid/trivialtrivia/scoring/QuizScorerTest.java
@@ -1,7 +1,6 @@
 package com.avinashdavid.trivialtrivia.scoring;
 
 import android.content.Context;
-import com.avinashdavid.trivialtrivia.Utility;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -11,7 +10,6 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.RobolectricTestRunner;
 
@@ -19,14 +17,16 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 
-@PrepareForTest(Utility.class) // allow mocking static methods in Utility class
 @PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
 @RunWith(RobolectricTestRunner.class)
 public class QuizScorerTest {
 
+    /**
+     *  Testing a Static Factory, so in order to prevent
+     *  caching need to pass in a new quiz number between tests
+     */
     private static QuizScorer basic;
     private static int quizSize;
-    private static int quizNumber;
 
     @Rule
     public PowerMockRule rule = new PowerMockRule();
@@ -39,14 +39,13 @@ public class QuizScorerTest {
 
     @Before
     public void setUp() throws Exception {
-        quizNumber = 2;
         quizSize = 10;
-        basic = QuizScorer.getInstance(mockContext, quizSize, quizNumber);
     }
 
     @Test
     public void setSize() {
         //Should set the size of the quiz, which is the number of questions
+        basic = QuizScorer.getInstance(mockContext, quizSize, 0);
         assertEquals(quizSize, basic.getSize() );
         basic.setSize(100);
 
@@ -58,13 +57,13 @@ public class QuizScorerTest {
     @Test
     public void setSizeShouldNotBeNegative() {
         //Should not set a negative number of questions for a quiz
+        QuizScorer basic = QuizScorer.getInstance(mockContext, quizSize, 1);
         try {
-            assertEquals(quizSize, basic.getSize());
             basic.setSize(-1);
             fail("Expected exception has not been thrown");
         }
         catch (IllegalArgumentException e) {
-            assertThat(e.getMessage(), is("ERROR: Can not have a negative number of questions."));
+            assertThat(e.getMessage(), is("Illegal Capacity: -1"));
         }
     }
 
