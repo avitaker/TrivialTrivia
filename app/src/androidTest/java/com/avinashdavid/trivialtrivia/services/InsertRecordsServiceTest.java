3b89af42b91f7b65aff26a674567864b22066277
Carrie <clipsong@gmail.com>
2020-05-18 10:33:13 -0700

    Add Insert Record Service Test

---
+++ app/src/androidTest/java/com/avinashdavid/trivialtrivia/services/InsertRecordsServiceTest.java
@@ -0,0 +1,12 @@
+package com.avinashdavid.trivialtrivia.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InsertRecordsServiceTest {
+
+    @Test
+    public void onHandleIntent() {
+    }
+}
