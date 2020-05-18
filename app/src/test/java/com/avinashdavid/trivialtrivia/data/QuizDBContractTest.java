5f4c6736446795273c36f39311c37a5e19716def
Carrie <clipsong@gmail.com>
2020-05-18 16:33:16 -0700

    Test the URI of the category index and the category total questions answered

--- app/src/test/java/com/avinashdavid/trivialtrivia/data/QuizDBContractTest.java
+++ app/src/test/java/com/avinashdavid/trivialtrivia/data/QuizDBContractTest.java
@@ -17,21 +17,32 @@ public class QuizDBContractTest {
     }
 
     @Test
-    public void testbuildUriQuizId(){
+    public void buildUriQuizId(){
         //buildUriQuizId Should Return the URI Needed
         assertEquals("content://com.avinashdavid.trivialtrivia.data/quiz/3", QuizDBContract.QuizEntry.buildUriQuizId(3).toString() );
     }
 
     @Test
-    public void testbuildUriCategoryId(){
+    public void buildUriCategoryId(){
         // buildUriCategoryId should return the expected URI
         assertEquals("content://com.avinashdavid.trivialtrivia.data/category/4", QuizDBContract.CategoryEntry.buildUriCategoryId(4).toString());
     }
 
     @Test
-    public void testbuildUriCategoryName(){
+    public void buildUriCategoryName(){
         // buildUriCategoryName should return the expected URI
         assertEquals("content://com.avinashdavid.trivialtrivia.data/category/1", QuizDBContract.CategoryEntry.buildUriCategoryName("general").toString());
     }
 
+    @Test
+    public void buildUriCategoryTotalQuestionsAnsweredForId(){
+        // total questions answered should return the expected URI
+        assertEquals("content://com.avinashdavid.trivialtrivia.data/category/categoryTotalQuestions/_id/3", QuizDBContract.CategoryEntry.buildUriCategoryTotalQuestionsAnsweredForId(3).toString());
+    }
+
+    @Test
+    public void buildUriCategoryIndex(){
+        // category index return the expected URI
+        assertEquals("content://com.avinashdavid.trivialtrivia.data/category/4", QuizDBContract.CategoryEntry.buildUriCategoryIndex(3).toString());
+    }
 }
